0cad3d2fa70d45ba4d765de5498581936fd83b6f
DominikSmidak <xsmidak@stuba.sk>
2023-05-18 17:40:57 +0200

    Preklad hlavnej stranky

---
+++ src/langs/sk.php
@@ -0,0 +1,12 @@
+<?php
+$lang = array(
+    "title" => "Hlavná Stránka",
+    "login" => "Prihlásenie",
+    "email" => "Email :)",
+    "invalid_email" => "Zadaj správny email.",
+    "password" => "Heslo",
+    "invalid_password" => "Zadaj správne heslo.",
+    "login_button" => "Prihlásiť"
+);
+
+?>
