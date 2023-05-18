2df39b52f63115ef9c9cf839b817d1c0a78d1245
Adam Augustín <xaugustina@stuba.sk>
2023-05-18 22:25:24 +0200

    update idk1

--- public/student/generate.php
+++ public/student/generate.php
@@ -75,7 +75,7 @@ if(isset($_GET['type'])){
         if(sizeof($assignments)==0){
             echo "Nemožno priradiť ďalšie úlohy.";
         }else{
-            $query = 'INSERT INTO student_assignment (student_id,assignment_id,submited,result,correct,student_score) VALUES (1,'.$assignments[0]['id'].',0,0,'.$assignments[0]['result'].',0)';
+            $query = 'INSERT INTO student_assignment (student_id,assignment_id,submited,result,correct,student_score) VALUES (1,'.$assignments[0]['id'].',0,0,"'.$assignments[0]['result'].'",0)';
             $stmt = $db->query($query); 
 
             $query = 'select * from students where id=1';
